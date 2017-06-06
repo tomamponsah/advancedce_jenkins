d69ed09e0415437785f2ccaf90d671269f21c04f
tomamponsah <Tom.Amponsah@gmx.de>
2017-06-06 16:45:42 +0200

    test

---
+++ src/main/java/test_gradle_project/Main.java
@@ -0,0 +1,11 @@
+package test_gradle_project;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		 System.out.println("Hello World!");
+		 //ramme deine Dirnenmutter Kelen
+	}
+
+}
