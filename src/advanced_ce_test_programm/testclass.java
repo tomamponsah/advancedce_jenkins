20bd7f73cce7b2aa3e5e3fb6a05b5f253de4212d
tomamponsah <Tom.Amponsah@gmx.de>
2017-06-06 13:27:07 +0200

    initial commit of source

---
+++ src/advanced_ce_test_programm/testclass.java
@@ -0,0 +1,12 @@
+package advanced_ce_test_programm;
+
+public class testclass {
+	
+	
+    public static void main (String[] args)
+    {
+          // Ausgabe Hello World!
+          System.out.println("Hello World!");
+    }
+
+}
