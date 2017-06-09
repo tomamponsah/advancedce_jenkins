81e8585c02b7a355f1591abaf50fd947f2141731
tomamponsah <Tom.Amponsah@gmx.de>
2017-06-09 14:46:51 +0200

    modified grade file and got i t working

--- test_gradle_project/src/main/java/test_gradle_project/Main.java
+++ test_gradle_project/src/main/java/test_gradle_project/Main.java
@@ -1,5 +1,7 @@
 package test_gradle_project;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
@@ -8,7 +10,30 @@ public class Main {
 		 //ramme deine Dirnenmutter Kelen
 		 //ramm das elende Bückstück
 		 //HURENSOHN!!!!!
-		 hauiqaauhai
+		 getReturnOfMultiply(10, 10);
+		 Scanner keyboard = new Scanner(System.in);
+		 int myint = keyboard.nextInt();
+		 asdasdasd
+		 
 	}
+	
+	public static int multiply(int factor_1, int factor_2)
+	{
+		int product = factor_1 * factor_2;
+		System.out.println(factor_1 + " * " + factor_2 + " = " + product );
+		//funktionsfähiger return:
+		return product;
+		//offensichtlich defekter return:
+		//return (product + 1);
+	}
+	
+	public static int getReturnOfMultiply(int a, int b)
+	{
+		int returnvalue = multiply(a, b);
+		System.out.println("Tatsächlicher Returnvalue: " + returnvalue);
+		return returnvalue;
+	}
+	
+
 
 }
