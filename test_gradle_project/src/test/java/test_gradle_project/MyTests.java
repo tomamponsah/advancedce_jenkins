7a002b33ad1078a6791d5a5ecf3351b70c37321f
tomamponsah <Tom.Amponsah@gmx.de>
2017-06-18 12:29:45 +0200

    test added

---
+++ test_gradle_project/src/test/java/test_gradle_project/MyTests.java
@@ -0,0 +1,17 @@
+package test_gradle_project;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class MyTests {
+
+    @Test
+    public void multiplicationOfZeroIntegersShouldReturnZero() {
+        Main tester = new Main(); // MyClass is tested
+
+        // assert statements
+        assertEquals("10 x 0 must be 0", 0, Main.multiply(10, 0));
+        assertEquals("0 x 10 must be 0", 0, Main.multiply(0, 10));
+        assertEquals("0 x 0 must be 0", 0, Main.multiply(0, 0));
+    }
+}
